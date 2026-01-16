fcf10b02886a37ec05336d50cba645e3f934351a
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-01-16 09:14:18 +0100

    navbar ready

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {backgroundColor: 'darkblue', borderTopColor: 'grey'},
+        tabBarActiveTintColor: 'lightblue',
+        tabBarInactiveTintColor: 'red',
+        headerStyle: {backgroundColor: 'yellow'},
+        headerTintColor: 'black'
+        }
+    }>
+      <Tabs.Screen name="index" 
+                  options={{title: "Foglalások", tabBarIcon:({color}) => <Ionicons name="calendar" size={24} color={color}/>}}/>
+      <Tabs.Screen name="post" 
+                  options={{title: "Új foglalás", tabBarIcon:({color}) => <Ionicons name="add-circle" size={24} color={color}/>}}/>
+    </Tabs>
+  )
+    
 }
